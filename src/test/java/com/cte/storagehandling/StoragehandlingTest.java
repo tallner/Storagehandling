7738f7eb047cbb2d0d9739ad0833491bcf9a246b
tallner <79505011+tallner@users.noreply.github.com>
2021-04-27 10:28:16 +0200

    test corrected

--- src/test/java/com/cte/storagehandling/StoragehandlingTest.java
+++ src/test/java/com/cte/storagehandling/StoragehandlingTest.java
@@ -13,14 +13,16 @@ public class StoragehandlingTest {
         storagehandling.setName("X20CP0292");
         storagehandling.setLocation("Stockholm, Johans skrivbord");
         storagehandling.setSerialnumber("2345A98JUT56");
-        storagehandling.setStatus("Internal tests until 2025.04.19");;
+        storagehandling.setStatus("Internal tests until 2025.04.19");
 
-        //assertThat(storagehandling.calculateCustomerPrice((float) addPercentageToPrice)).isEqualTo(720);
+        assertThat(storagehandling.getStatus().compareTo("Internal tests until 2025.04.19"));
+        
+        
     }
-    
+/** /
     @Test
     void calculateCustomerPrice_checkValueFloat(){
-        float addPercentageToPrice = 20;
+        //float addPercentageToPrice = 20;
         Storagehandling storagehandling = new Storagehandling();
         storagehandling.setName("X20CP0292");
      //   storagehandling.setlink("https://www.br-automation.com/en/products/x20cp0292/");
@@ -28,7 +30,7 @@ public class StoragehandlingTest {
 
        // assertThat(storagehandling.calculateCustomerPrice(addPercentageToPrice)).isEqualTo(111.6f);
     }
-
+/**/
     
     
 }
